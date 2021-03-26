6fc41a95144acc2db2c6e3793436b85ab0dca42d
Néstor G. Trillo <nospam@avaniu.com>
2021-03-26 11:21:19 -1000

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Nestor Trillo on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
